6725e0024862ae2c5e89a0bac185ff825a367891
Strykerdred <Ninovansaaze@gmail.com>
2025-02-10 10:43:59 +0100

    Update Tower WoF.cs

--- Assets/Tower WoF.cs
+++ Assets/Tower WoF.cs
@@ -7,7 +7,7 @@ public class TowerWoF : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //black moneys
     }
 
     // Update is called once per frame
