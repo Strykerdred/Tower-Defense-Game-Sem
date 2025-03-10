0622a41c94e74b5b0695f95a591446a789835a33
overlord <semvh0306@gmail.com>
2025-03-10 12:40:46 +0100

    shop rewrok

---
+++ Assets/Shop.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Shop : MonoBehaviour {
+
+    public void PurchaseWoF ()
+    {
+        Debug.Log("Wall of Flesh summoned");
+    }
+    public void PurchasePyro ()
+    {
+        Debug.Log("The Pyro has spawned in");
+    }
+    public void PurchaseClodsire()
+    {
+        Debug.Log("Trainer sent out Clodsire");
+    }
+    public void PurchaseMasterChief()
+    {
+        Debug.Log("THE Master Chief himself has arrived");
+    }
+    public void PurchaseFrozone()
+    {
+        Debug.Log("Frozone slides into battle");
+    }
+
+}
